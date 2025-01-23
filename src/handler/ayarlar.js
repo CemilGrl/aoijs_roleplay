6ea65516a6dd69c35cb07f31bf00ce882ef99c3a
ArseN <77533226+CemilGrl@users.noreply.github.com>
2025-01-23 13:15:23 +0300

    Update ayarlar.js

--- src/handler/ayarlar.js
+++ src/handler/ayarlar.js
@@ -1,7 +1,7 @@
 module.exports = {
     hex: "a4d3ee",
-    tick: "<a:arsen_tik:1331855644030140447>",
-    cross: "<a:kilit:1331856342947725442>",
+    tick: "✅",
+    cross: "❎",
 
     //roleplay
 
@@ -9,14 +9,14 @@ module.exports = {
     xp: 0n,
     rpsüre: "",
     enson: "",
-    rpkategori: "1331883494099648594",
+    rpkategori: "", //kategoriid1;kategoriid2;kategoriid3...
 
     //ekonomi
 
     para: 0n,
 
-    //loglar
-    paralog: "1331884004177346615",
-    rplog: "1331883858894782487",
-    geçişlog: "1331883924451885098"
+    //loglar (Sadece id yazmanız yeterli.)
+    paralog: "",
+    rplog: "",
+    geçişlog: ""
 }
